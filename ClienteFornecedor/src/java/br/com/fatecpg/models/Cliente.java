dc00c6bf8d1b7ba630eea614ff0a3084d84b67ca
Victor Schleumer <victor.schleumer@gmail.com>
2017-04-29 10:16:40 -0300

    Classe Cliente

--- ClienteFornecedor/src/java/br/com/fatecpg/models/Cliente.java
+++ ClienteFornecedor/src/java/br/com/fatecpg/models/Cliente.java
@@ -5,10 +5,212 @@
  */
 package br.com.fatecpg.models;
 
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  *
- * @author jeffersoncn
+ * @author Victor Schleumer
  */
 public class Cliente {
+
+    private static ArrayList<Cliente> lista;
+
+    private int id;
+    private String nome;
+    private String cpf;
+    private String rg;
+    private String email;
+    private String telefone;
+    private String endereco;
+
+    private static final String EMAIL_PATTERN
+            = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+            + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
     
+    private static final Pattern pattern = Pattern.compile(EMAIL_PATTERN, Pattern.CASE_INSENSITIVE);
+
+    public ArrayList<Cliente> listar() {
+        return lista;
+    }
+
+    public int adicionar(Cliente cliente) {
+        try {
+            lista.add(cliente);
+            return cliente.id;
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            return 0;
+        }
+    }
+
+    public Cliente clienteById(int id) {
+        try {
+            for (Cliente cliente : lista) {
+                if (cliente.id == id) {
+                    return cliente;
+                }
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            return null;
+        }
+        return null;
+    }
+
+    public boolean editar(Cliente dados, int id) {
+        try {
+            for (Cliente cliente : lista) {
+                if (cliente.id == id) {
+                    cliente = dados;
+                    return true;
+                }
+            }
+            return true;
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            return false;
+        }
+    }
+
+    public boolean excluir(int id) {
+        try {
+            for (Cliente cliente : lista) {
+                if (cliente.id == id) {
+                    lista.remove(cliente);
+                    return true;
+                }
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            return false;
+        }
+        return false;
+    }
+
+    public Cliente(String nome, String cpf, String rg, String email, String telefone, String endereco) {
+        try {
+            if (cpf.equals("00000000000") || cpf.equals("11111111111")
+                    || cpf.equals("22222222222") || cpf.equals("33333333333")
+                    || cpf.equals("44444444444") || cpf.equals("55555555555")
+                    || cpf.equals("66666666666") || cpf.equals("77777777777")
+                    || cpf.equals("88888888888") || cpf.equals("99999999999")
+                    || (cpf.length() != 11)) {
+                throw new Exception("CPF Inválido");
+            }
+
+            char dig10, dig11;
+            int sm, i, r, num, peso;
+
+            sm = 0;
+            peso = 10;
+            for (i = 0; i < 9; i++) {
+                num = (int) (cpf.charAt(i) - 48);
+                sm = sm + (num * peso);
+                peso = peso - 1;
+            }
+
+            r = 11 - (sm % 11);
+            if ((r == 10) || (r == 11)) {
+                dig10 = '0';
+            } else {
+                dig10 = (char) (r + 48); // converte no respectivo caractere numerico
+            }
+            sm = 0;
+            peso = 11;
+            for (i = 0; i < 10; i++) {
+                num = (int) (cpf.charAt(i) - 48);
+                sm = sm + (num * peso);
+                peso = peso - 1;
+            }
+
+            r = 11 - (sm % 11);
+            if ((r == 10) || (r == 11)) {
+                dig11 = '0';
+            } else {
+                dig11 = (char) (r + 48);
+            }
+
+            if (!(dig10 == cpf.charAt(9)) && (dig11 == cpf.charAt(10))) {
+                throw new Exception("CPF Inválido");
+            }
+
+            cpf = (cpf.substring(0, 3) + "." + cpf.substring(3, 6) + "."
+                    + cpf.substring(6, 9) + "-" + cpf.substring(9, 11));
+            
+            Matcher matcher = pattern.matcher(email);
+            if(!matcher.matches()){
+                throw new Exception("E-mail Inválido");
+            }
+            
+            this.id = lista.size() + 1;
+            this.nome = nome;
+            this.cpf = cpf;
+            this.rg = rg;
+            this.email = email;
+            this.telefone = telefone;
+            this.endereco = endereco;
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
 }
